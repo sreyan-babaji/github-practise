6cb1925c5e53a3184d835f9d236f1dc8afd1a511
sreyan-babaji <sinhasreyan744@gmail.com>
2024-08-26 21:17:39 +0600

    first code

---
+++ index.php
@@ -0,0 +1,19 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <title>PHP AND  HTML</title>
+    <link rel="stylesheet" href="asset/css/bootstrap.min.css">
+    <link rel="stylesheet" href="asset/css/style.css">
+    <style>
+	
+    </style>
+</head>
+<body>
+
+  Home
+
+  <script src="asset/js/bootstrap.min.js"></script>
+</body>
+</html>
